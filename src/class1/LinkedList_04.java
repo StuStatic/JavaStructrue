87416c056c24afc08c966d30790c922e4d585b93
程丕砚 <14722828229@163.com>
2021-01-30 17:16:40 +0800

    ListNode function:revert()

--- src/class1/LinkedList_04.java
+++ src/class1/LinkedList_04.java
@@ -7,66 +7,58 @@ public class LinkedList_04 {
 
     public static void main(String[] args) {
         //创建数据
-//        Node head = new Node();
-//        Node node1 = new Node("节点01", head);
-//        Node node2 = new Node("节点02", head);
-//        Node node3 = new Node("节点03", head);
-//        Node node4 = new Node("节点04", head);
-//        head.next = node1;
-//        node1.next = node2;
-//        node2.next = node3;
-//        node3.next = node4;
-//        node4.next = null;
+        ListNode head = new ListNode();
+        ListNode listNode1 = new ListNode("节点01", head);
+        ListNode listNode2 = new ListNode("节点02", head);
+        ListNode listNode3 = new ListNode("节点03", head);
+        ListNode listNode4 = new ListNode("节点04", head);
+        head.next = listNode1;
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = null;
         //测试遍历
-//        new Node(null).print();
+//        new ListNode(null).print();
 
-        //测试链表尾部添加,结果应为node1-node2-node3-node4-node5
-//        Node node5 = new Node("节点05", head);
-//        Node.add(node5);
-//        Node.print();
+        //测试链表尾部添加,结果应为ListNode1-ListNode2-ListNode3-ListNode4-ListNode5
+//        ListNode listNode5 = new ListNode("节点05", head);
+//        ListNode.add(listNode5);
+//        ListNode.print();
 
-        //测试在pos=3的位置添加，结果应为node1-node2-node5-node3-node4
-//        Node node5 = new Node("节点05", head);
-//        Node.addByOrder(node5, 3);
-//        Node.print();
+        //测试在pos=3的位置添加，结果应为ListNode1-ListNode2-ListNode5-ListNode3-ListNode4
+//        ListNode listNode5 = new ListNode("节点05", head);
+//        ListNode.addByOrder(listNode5, 3);
+//        ListNode.print();
 
         //测试链表反转
-        Node node1 = new Node("节点01");
-        Node node2 = new Node("节点02");
-        Node node3 = new Node("节点03");
-        Node node4 = new Node("节点04");
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = null;
-        Node.reverse(node1);
-//        Node.print();
+        ListNode listNode = ListNode.reverse(head);
+        ListNode.revertPrint(listNode);
     }
 }
 
-//定义一个节点Node，每个节点包括数据域和指针域
-class Node {
+//定义一个节点ListNode，每个节点包括数据域和指针域
+class ListNode {
     public String data;
-    public Node next;//指向下一个节点
-    public static Node head;
+    public ListNode next;//指向下一个节点
+    public static ListNode head;
 
-    public Node(String data, Node head) {
+    public ListNode(String data, ListNode head) {
         this.data = data;
         this.head = head;
     }
 
-    public Node(String data) {
+    public ListNode(String data) {
         this.data = data;
     }
 
-    public Node() {
+    public ListNode() {
 
     }
 
     //在链表尾部直接添加节点
-    public static void add(Node node) {
+    public static void add(ListNode listNode) {
         //1创建temp节点
-        Node temp = head;
+        ListNode temp = head;
         //2遍历链表
         while (true) {
             if (temp.next == null) {
@@ -75,16 +67,16 @@ class Node {
                 temp = temp.next;
             }
         }
-        temp.next = node;
+        temp.next = listNode;
     }
 
     //在链表指定位置添加节点
-    //节点对应pos:head-0;node1-1;node2-2
-    public static void addByOrder(Node node, int position) {
+    //节点对应pos:head-0;ListNode1-1;ListNode2-2
+    public static void addByOrder(ListNode listNode, int position) {
         //设置一个标识位
         int pos = 0;
         //设置temp
-        Node temp = head;
+        ListNode temp = head;
         //添加到指定位置的前一个节点
         while (true) {
             if (temp.next == null) {
@@ -93,36 +85,28 @@ class Node {
             pos++;
             temp = temp.next;
             if (pos == position - 1) {
-                node.next = temp.next;
-                temp.next = node;
+                listNode.next = temp.next;
+                temp.next = listNode;
             }
         }
     }
 
     //链表反转
-    //先找到node1,node2->temp=node2->temp.next=node1->node3 = node2.next
-    public static void reverse(Node node) {
-        Node temp = node;//控制链表顺序遍历
-        Node revert;//链表反转所用中间变量
-        while (true) {
-            if (temp == null) {
-                break;
-            }
-            revert = temp.next;
-//            System.out.println("revert.data：" + revert.data);
-//            if (temp.data == null) {
-//                revert.next = null;
-//            } else {
-//                revert.next = temp;
-//                if (revert.next.data != null)
-//                    System.out.println("revert.next.data：" + revert.next.data);
-//            }
-            System.out.println("当前节点：" + temp.data);
-            temp = temp.next;
+    public static ListNode reverse(ListNode head) {
+        ListNode front = head;//01
+        ListNode after = head.next;//02
+        ListNode temp;
+        while (after != null) {
+            temp = after.next;//03
+            after.next = front;//02->01
+            front = after;//01
+            after = temp;
         }
+        head.next = null;
+        return front;
     }
 
-    //打印所有节点
+    //打印所有节点(不包括链表反转方法的打印)
     public static void print() {
         //1先判断链表是否为null
         if (head.next == null) {
@@ -130,7 +114,7 @@ class Node {
             return;
         }
         //2需要一个辅助temp节点来获取每个节点
-        Node temp = head.next;
+        ListNode temp = head.next;
         while (true) {
             //判断是否到链表最后
             if (temp == null) {
@@ -141,4 +125,17 @@ class Node {
             temp = temp.next;
         }
     }
+
+    public static void revertPrint(ListNode listNode){
+        ListNode temp = listNode;//遍历用到的临时变量
+        while (true) {
+            if(temp.next == null){
+                break;
+            }
+            System.out.println(temp.data);
+            //将temp后移
+            temp = temp.next;
+        }
+    }
+
 }
